068589ac58fc06fdd51f1a5c138053b6c7b4b3e6
vashi9 <37874769+vashi9@users.noreply.github.com>
2019-06-25 12:06:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5002))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('HELLO RUPEEK!')
+  response.send('WELCOME TO RUPEEK!')
 })
 
 app.listen(app.get('port'), function() {
